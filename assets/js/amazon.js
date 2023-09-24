0a51eb2677a36c4857149012a849fa54478baa66
KAVIRAYAR S <137653439+vasanth-kavi@users.noreply.github.com>
2023-09-25 00:59:25 +0530

    my own site

---
+++ assets/js/amazon.js
@@ -0,0 +1,11 @@
+function allMenu(){
+    document.getElementById('all-list').style="display:block;box-shadow: 0px 0 10px 1450px rgba(0,0,0,.7);";  
+
+    document.getElementById('body').style="position: fixed";  
+  }
+  function closeAll(){
+    console.log('---------------------------sn');
+    document.getElementById('all-list').style="display:none";
+    document.getElementById('body').style="position: relative";  
+
+  }
